dcc88a28ff32489bcf16bba7f5e1d2df4db61635
Elia Mascolo <eliamascolo94@gmail.com>
2022-05-15 00:37:05 -0400

    [FEAT] Genome class now owns all the positional distribution methods + new code for the intergenicity analysis.

--- src/genome.py
+++ src/genome.py
@@ -42,7 +42,8 @@ class Genome():
         self.norm_gini = None
         self.evenness = None
         self.new_evenness = None
-        
+        self.intergenicity = None
+    
     def scan(self, motif, pseudocount, threshold=None):
         pwm = motif.counts.normalize(pseudocounts=pseudocount)
         rpwm = pwm.reverse_complement()
@@ -62,7 +63,8 @@ class Genome():
             hits_positions = np.argwhere(effective_scores > threshold).flatten()
             self.hits = {'scores': hits_scores,
                          'positions': hits_positions,
-                         'threshold': threshold}
+                         'threshold': threshold,
+                         'motif_length': pssm.length}
         
     def combine_f_and_r_scores(self, f_scores, r_scores):
         '''
@@ -302,7 +304,6 @@ class Genome():
                 'scan' method specifying a threshold to get PSSM-hits before\
                 calling 'analyze_positional_distribution'.")
         
-        
         # Number of predicted binding sites
         self.n_sites = len(self.hits['scores'])
         # Site density (sites per thousand bp)
@@ -340,9 +341,182 @@ class Genome():
         # Set original evenness and new evenness
         self.evenness = self.get_original_evenness()
         self.new_evenness = self.get_new_evenness()
+    
+    def overlaps_with_feature(self, site_pos, feat):
+        '''
+        Given a genome feature and a TF binding site position, it returns True
+        if there is overlap, False otherwise.
+        '''
         
+        site_start = site_pos
+        site_end = site_start + self.hits['motif_length']
+        
+        feat_start = int(feat.location.start)
+        feat_end = int(feat.location.end)
+        
+        if site_start < feat_end and feat_start < site_end:
+            return True
+        else:
+            return False
+    
+    def gene_to_site_distance(self, feat, site_pos, circular_genome=False):
+        '''
+        
+        Returns
+        -------
+        result : DICT
+            The function returns a dictionary with two keys: 'distance' and 'location'.
+            The 'distance' key provides an integer. Its absolute value is the distance
+            between the gene start position and the closest edge of the TFBS. It's
+            going to be 0 if the gene start is contained into the TFBS; it's going
+            to be negative for TFBS that are upstream of the gene start; it's going
+            to be positive for TFBS that are downstream of the gene start.
+            The 'location' key is redundant, and it provides a string indicating
+            whether the TFBS is located upstream or downstream of the gene start.
+            
+            
+            EXAMPLE 1:
+                A 23 bp TFBS located at position 1000, will be reported to be +3 bp
+                from a gene start at position 997, -5 bp from a gene start at
+                position 1028, and 0 bp from a gene start at position 1016 (because
+                the TFBS would contain the gene start).
+            
+            EXAMPLE 2:
+                In a circular genome of 1,000,000 bp a TFBS located at position
+                1000 would be reported to be at +1030 bp from a gene start located
+                at position 999,970.
+            
+        '''
+        
+        # Define site center "position" (it can be non-integer)
+        edge_to_center = (self.hits['motif_length'] - 1)/2
+        site_center = site_pos + edge_to_center
+        
+        # Feature coordinates
+        coord = np.array([int(feat.location.start), int(feat.location.end)])
+        
+        # Three pairs of coordinates for circular genomes. One pair otherwise.
+        coordinates = [coord]
+        if circular_genome == True:
+            coordinates.append(coord + self.length)
+            coordinates.append(coord - self.length)
+        
+        # In this list, a single distance will be appended for non circular
+        # genomes. For circular genomes three distances will be recorded (for
+        # the three coordinate systems)
+        tmp_distances = []
+        
+        for coord in coordinates:
+            # Identify gene start position and compute distance from site_center
+            
+            # If gene is on forward strand
+            if feat.location.strand in [1, '1', '+']:
+                gene_start = coord[0]
+                tmp_distances.append(site_center - gene_start)
+            
+            # If gene is on reverse strand
+            elif feat.location.strand in [-1, '-1', '-']:
+                gene_start = coord[1]
+                tmp_distances.append(gene_start - site_center)
+            
+            else:
+                raise ValueError("Unknown 'location.strand' value: " +
+                                 str(feat.location.strand))
+        
+        # Choose the distance with the lowest absolute value.
+        tmp_absolute_distances = [abs(x) for x in tmp_distances]
+        gene_to_site_center = tmp_distances[np.argmin(tmp_absolute_distances)]
+        
+        # Define distance
+        if abs(gene_to_site_center) < edge_to_center:
+            # Overlapping
+            distance = 0
+        else:
+            # Reduce the absoulte value of the distance by  edge_to_center
+            if gene_to_site_center > 0:
+                # Downstream
+                distance = round(gene_to_site_center - edge_to_center)
+            else:
+                # Upstream
+                distance = round(gene_to_site_center + edge_to_center)
+        
+        return distance
+    
+    def get_genetic_context(self, site_pos):
+        
+        intergenic = True
+        
+        genes = []
+        distances = []
+        for feat in self.features:
+            # Ignore the feature if it's not 'CDS' nor 'tRNA' nor 'rRNA'
+            if feat.type not in ['CDS', 'tRNA', 'rRNA']:  # !!! other feat types ...
+                continue
+            
+            # Distance (from gene start to site)
+            distance = self.gene_to_site_distance(
+                feat, site_pos, circular_genome=True)  # !!! circular genome?
+            
+            # As soon as one gene is found to overlap with the hit, the
+            # variable  intergenic  is set to False.
+            if self.overlaps_with_feature(site_pos, feat):
+                intergenic = False
+            
+            genes.append(feat)
+            distances.append(distance)
+        
+        if len(genes) > 0:
+            # Get closest gene record
+            abs_distances = [abs(x) for x in distances]
+            j = np.argmin(abs_distances)  # j-th element was the closest gene
+            closest_gene = genes[j]  # j-th gene was the closest gene
+        else:
+            intergenic = 'no_genes'
+            closest_gene = 'no_genes'
+        
+        return intergenic, closest_gene
+    
+    def set_hits_genetic_context(self):
+        
+        if not self.hits:
+            raise TypeError(
+                "The 'hits' attribute is 'NoneType'. Make sure you call the\
+                'scan' method specifying a threshold to get PSSM-hits before\
+                calling 'get_intergenicity'.")
+        
+        intergenic_list = []
+        closest_gene_list = []
+        for hit_pos in self.hits['positions']:
+            intergenic, closest_gene = self.get_genetic_context(hit_pos)
+            intergenic_list.append(intergenic)
+            closest_gene_list.append(closest_gene)
+        self.hits['intergenic'] = intergenic_list
+        self.hits['closest_genes'] = closest_gene_list
+    
+    def analyze_intergenicity(self):
+        
+        if not self.hits:
+            raise TypeError(
+                "The 'hits' attribute is 'NoneType'. Make sure you call the\
+                'scan' method specifying a threshold to get PSSM-hits before\
+                calling 'get_intergenicity'.")
+        
+        # Identify genetic context for each hit
+        self.set_hits_genetic_context()
+        
+        if self.n_sites == 0:
+            return 'no_hits'
+        
+        if self.hits['intergenic'][0] == 'no_genes':
+            return 'no_genes'
+        
+        # Count number of intergenic sites
+        n_intergenic = sum(self.hits['intergenic'])
+        # Intergenic frequency
+        intergenic_freq = n_intergenic / self.n_sites
+        # Set intergenicity attribute
+        self.intergenicity = intergenic_freq
 
-        
 
 
 
