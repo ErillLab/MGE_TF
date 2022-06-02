02edc50af23b921e571392a47706b2e294c735c8
Elia Mascolo <eliamascolo94@gmail.com>
2022-06-02 00:07:51 -0400

    MGE_TF: Add docstrings.

--- src/mge_tf.py
+++ src/mge_tf.py
@@ -17,17 +17,24 @@ class MGE_TF():
         self.motif_specific_vals = None
         
         # p-values
-        self.avg_score
-        self.extremeness
-        self.entropy
-        self.norm_entropy
-        self.gini
-        self.norm_gini
-        self.evenness
-        self.new_evenness
-        self.intergenicity
+        self.avg_score = None
+        self.extremeness = None
+        self.entropy = None
+        self.norm_entropy = None
+        self.gini = None
+        self.norm_gini = None
+        self.evenness = None
+        self.new_evenness = None
+        self.intergenicity = None
     
     def compute_motif_specific_vals(self):
+        '''
+        Each motif in the TF object (the original motif and the permuted motifs)
+        is used to analyze the MGE object. The results for each motif are
+        stored in the motif_specific_vals attribute.
+        motif_specific_vals is a dictionary where the keys are the different
+        metrics, and the values are lists with the results from all the motifs.
+        '''
         
         stats = ['avg_score', 'extremeness', 'entropy', 'norm_entropy', 'gini',
                  'norm_gini', 'evenness', 'new_evenness', 'intergenicity']
@@ -43,9 +50,14 @@ class MGE_TF():
             self.compile_motif_specific_vals(stats)
     
     def init_motif_specific_vals(self, stats):
+        ''' Initialize motif_specific_vals attribute as a dictionary where the
+        values are empty lists. '''
         self.motif_specific_vals = dict(zip(stats, [[]]*len(stats)))
     
     def compile_motif_specific_vals(self, stats):
+        ''' Compile motif_specific_vals attribute. The current statistic values
+        from the MGE object are appended to the corresponding values of the
+        motif_specific_vals dictionary. '''
         for stat in stats:
             val = vars(self.mge)[stat]
             self.motif_specific_vals[stat].append(val)
